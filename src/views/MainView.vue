7c999b02798a172112fa15aa640a019edbb36f81
Phil Gehrmann <phil.gehrmann@gmail.com>
2022-11-28 07:08:43 +0100

    add navbtn

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,15 +1,12 @@
-<script setup lang="ts">
+<script setup>
     import { RouterLink, RouterView } from "vue-router";
     import HeaderComponent from "../components/HeaderComponent.vue";
-    import AudioPlayerComponent from "../components/AudioplayerComponent.vue";
 </script>
 
 <template>
     <main className="main__content">
       <HeaderComponent />
-      <h1>WELCOME</h1>
       <RouterView />
-      <AudioPlayerComponent />
     </main>
 </template>
 
